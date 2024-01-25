2880ba222fd6d530cb69700c5f433045b3f0d721
Aaryan <jainaryan24111@gmail.com>
2024-01-25 11:16:53 +0530

    Firebase Databse

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,10 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 
 Future<void> main() async{
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(const MyApp());
 
 }
